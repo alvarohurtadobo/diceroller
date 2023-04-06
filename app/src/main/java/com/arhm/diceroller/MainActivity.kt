20f3630ae325d15a9f7c67653b9bf7e58e6da590
Álvaro Rubén Hurtado Maldonado <a.hurtado.bo@gmail.com>
2023-04-06 19:28:30 -0400

    Finished first lesson

--- app/src/main/java/com/arhm/diceroller/MainActivity.kt
+++ app/src/main/java/com/arhm/diceroller/MainActivity.kt
@@ -3,11 +3,13 @@ package com.arhm.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,11 +20,23 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Presionado segundo", Toast.LENGTH_SHORT).show()
             rollDice()
         }
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
         val resultText : TextView = findViewById(R.id.result_text_view)
         val randomInt = Random().nextInt(6)+1
         resultText.text = "Dice rolled $randomInt"
+
+        val drawableResource = when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 }
