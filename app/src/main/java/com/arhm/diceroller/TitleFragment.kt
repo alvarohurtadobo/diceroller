2214c2d278af7f01a24e9388bfa83007a88a7dbd
Álvaro Rubén Hurtado Maldonado <a.hurtado.bo@gmail.com>
2023-04-06 20:19:07 -0400

    Added fragment, about to add binding

---
+++ app/src/main/java/com/arhm/diceroller/TitleFragment.kt
@@ -0,0 +1,47 @@
+package com.arhm.diceroller
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+//import androidx.databinding.DataBindingUtil
+
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TitleFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_title, container, false)
+//        val binding: FragmentTitleBinding = DataBindingUtil.in
+    }
+
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment TitleFragment.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            TitleFragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
+}
